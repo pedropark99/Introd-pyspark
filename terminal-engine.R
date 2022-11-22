0c6b24c8a37aa32e31a93b5e318f3c5a52ceea2c
pedropark99 <pedropark99@gmail.com>
2022-11-21 22:14:43 -0300

    Add a test Rmd to test the terminal engine

--- terminal-engine.R
+++ terminal-engine.R
@@ -1,5 +1,15 @@
 
 
 knitr::knit_engines$set(terminal = function(options){
-  
+  code <- paste(options$code, collapse = "\n")
+  code_to_run <- stringr::str_replace(
+    code, "^Terminal\\$ ", ""
+  )
+  code <- stringr::str_replace(
+    code, "^Terminal\\$ ",
+    '\n<span class="im">Terminal$</span>\n'
+  )
+  out <- system(code_to_run)
+
+  knitr::engine_output(options, code, out)
 })
