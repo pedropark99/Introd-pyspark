f71f125b56176f823a20e25b2f309f53ac99b045
pedropark99 <pedropark99@gmail.com>
2022-11-27 11:35:47 -0300

    Add function to print big lists

---
+++ Scripts/print_big_list.py
@@ -0,0 +1,25 @@
+def print_big_list(x, cut = 30):
+  if len(x) < 10:
+    print(x)
+  elif len(str(x)) < cut:
+    print(x)
+  else:
+    text = str(x)
+    print_text(text, cut)
+    
+def print_text(x, cut):
+  words = x.split(', ')
+  n = len(words)
+  lengths = [len(word) for word in words]
+  n_words_per_line = 4
+  index = 0
+  lines = list()
+  while index < n:
+    upper_limit = index + n_words_per_line
+    text = words[index:upper_limit]
+    text = ', '.join(text) + ',\n'
+    lines.append(text)
+    index = index + n_words_per_line
+    
+  text = ''.join(lines)
+  print(text)
