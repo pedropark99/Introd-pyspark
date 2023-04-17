8c3e0b986aa6ba9199607e3561a211d383cbcaf2
pedropark99 <pedropark99@gmail.com>
2023-04-16 22:00:26 -0300

    Update terminal engine script

--- Scripts/terminal-engine.R
+++ Scripts/terminal-engine.R
@@ -2,14 +2,25 @@
 
 knitr::knit_engines$set(terminal = function(options) {
   code <- paste(options$code, collapse = "\n")
-  code_to_run <- stringr::str_replace(
-    code, "^Terminal\\$ ", ""
+  code_to_run <- stringr::str_replace_all(
+    code, "Terminal\\$ ", ""
   )
-  code <- stringr::str_replace(
-    code, "^Terminal\\$ ",
-    '\n<span class="im">Terminal$</span>\n'
-  )
-  out <- system(code_to_run)
+  code_to_run <- paste0("bash -c ", shQuote(code_to_run), collapse = "")
+  out <- system(code_to_run, intern = TRUE)
+  out <- out[!is_log_info(out)]
 
+#   code <- stringr::str_replace_all(
+#     code, "Terminal\\$ ",
+#     '\n<span class="im">Terminal$</span>\n'
+#   )
+  
   knitr::engine_output(options, code, out)
 })
+
+
+is_log_info <- function(output) {
+    log_regex <- "[0-9]{2}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} WARN|\\[Stage "
+    stringr::str_detect(output, log_regex)
+}
+
+
