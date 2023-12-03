86fdeb65f8da79e0408caa7760dae364c38692e6
pedropark99 <pedropark99@gmail.com>
2023-12-03 13:53:39 -0300

    Add content

--- Scripts/clean_output_stages.py
+++ Scripts/clean_output_stages.py
@@ -1,41 +1,45 @@
-import os
 import re
+from pathlib import Path
 from bs4 import BeautifulSoup
 
+
+def get_html_chapter_files(folder):
+    chapters_files = [x for x in Path(folder).iterdir()]
+    return [str(x) for x in chapters_files if x.is_file() and x.name.endswith('html')]
+
 chapters_folder = "./docs/Chapters/"
-chapters_files = os.listdir(chapters_folder)
-chapters_files = [chapters_folder + f for f in chapters_files]
-
-is_html = lambda x: x.endswith(".html")
-chapters_files = list(filter(is_html, chapters_files))
-
-def read_file(path:str) -> list[str]:
-    with open(path, encoding="utf8") as f:
-        file = f.readlines()
-    return file
+chapters_files = get_html_chapter_files(chapters_folder)
 
 
-html_file = read_file(chapters_files[6])
-html_file = ''.join(html_file)
-html_file = BeautifulSoup(html_file, features="html.parser")
+def read_file(path):
+    with open(path, mode = 'r', encoding = "utf8") as file_connection:
+        content = file_connection.readlines()
+    return content
+
+def read_html_file(path):
+    text_file = read_file(path)
+    text_file = ''.join(text_file)
+    return BeautifulSoup(text_file, features="html.parser")
 
 
 def remove_stages(html_file):
     for node in html_file.find_all("code"):
         text = node.text
-        pattern = re.compile("\\[Stage ")
+        pattern = re.compile("^\\[Stage ")
         if pattern.match(text):
             node.decompose()
     
     return html_file
 
-# nodes_with_stage = list()
-# for node in code_nodes:
-#     text = node.text
-#     if have_stage(text):
-#         nodes_with_stage.append(node)
 
-html_file = remove_stages(html_file)
+def rewrite_without_stages(path):
+    html_file = read_html_file(path)
+    html_file = remove_stages(html_file)
+    with open(path, "w", encoding = "utf8") as file_connection:
+        file_connection.write(str(html_file))
+    return
 
-with open(chapters_files[6], "w", encoding="utf8") as file:
-    file.write(str(html_file))
+
+for path in chapters_files:
+    print(f"[INFO]: Rewriting {path}...")
+    rewrite_without_stages(path)
